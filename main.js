3305866820818413fe2c7b8a5b4b989955e09652
Daria Sibova <dariasib05@gmail.com>
2024-10-01 17:42:06 +0300

    commit

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 // Menu
 let menu = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
+
 
 
 menu.onclick = () => {
     menu.classList.toggle('move')
+    navbar.classList.toggle('open-menu')
+
 }
